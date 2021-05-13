c0fcde7b10e606d6cc98ecf12a817ea9cad67a23
WadeSpectre <mbergesjulien@gmail.com>
2021-05-13 09:29:59 +0200

    add useEffect for doc title

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
+
 import BackButton from "../components/BackButton";
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 
 function Contact() {
+    useEffect(() => {
+        document.title = "DEA - Contact";
+    }, []);
     return (
         <div className="flex flex-col relative">
             <BackButton />
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
+
 import "../styles/Home.css";
+
 import SocialNav from "../components/SocialNav";
 import DeaLogo from "../components/DeaLogo";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 function Home() {
+    useEffect(() => {
+        document.title = "DEA - Accueil";
+    }, []);
+
     return (
         <main className="min-h-screen flex flex-col items-center justify-center relative">
             <SocialNav />
