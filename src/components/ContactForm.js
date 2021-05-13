0c248aaeeb6e7a652b0690576a6e6bb178e3b120
WadeSpectre <mbergesjulien@gmail.com>
2021-05-13 09:06:29 +0200

    form fixed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,6 +9,7 @@ export default function ContactForm() {
 
         form.addEventListener("submit", function (e) {
             const formData = new FormData(form);
+            result.style.display = "block";
             e.preventDefault();
             var object = {};
             formData.forEach((value, key) => {
@@ -55,19 +56,14 @@ export default function ContactForm() {
         <div className="flex flex-col justify-center items-center w-full mb-5">
             <h1 className="m-4 blackops text-4xl md:text-7xl">CONTACT</h1>
             <form id="form" action="https://api.web3forms.com/submit" method="POST" className=" border border-red-600 rounded-xl p-2 w-11/12 md:w-2/4 mb-10">
-                <input type="hidden" name="apikey" value="9393c877-26c2-4c53-9da3-fa7e5c1aeb52" />
+                <input type="hidden" name="apikey" value="1d3a0cf6-94b4-487e-95f8-5c962e575e2e" />
                 <input type="hidden" name="subject" value="DEA - Nouveau message sur votre site" />
                 <Input regex label="Nom" id="lastName" placeholder="Nom" required></Input>
                 <Input regex label="Prénom" id="name" placeholder="Prénom" required></Input>
                 <Input type="email" label="Email" id="email" placeholder="Email" required />
                 <Input label="Téléphone(facultatif)" id="phone" type="phone" placeholder="Téléphone"></Input>
-                <Input textArea label="Message" id="message" placeholder="Message" required />
-                <div className="flex flex-col p-4">
-                    <button className="w-full px-3 py-4 text-white border-2 border-red-500 bg-red-700 rounded-md focus:bg-red-800 focus:outline-none blackops" type="submit">
-                        ENVOYER
-                    </button>
-                    <div id="result"></div>
-                </div>
+                <Input textArea label="Message" id="message" placeholder="Message" required lastOne />
+                <div id="result" className="text-center text-xl blackops"></div>
             </form>
         </div>
     );
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRef } from "react";
 
 const Input = (props) => {
     const [inputValue, setInputValue] = useState("");
-    const { id, wrapperClassName = "", regex, textArea, placeholder = "", label = "", type = "text", error = false, errorText = "", required, rows = "5", ...rest } = props;
+    const [isRequired, setIsRequired] = useState("");
+    const {
+        id,
+        wrapperClassName = "",
+        regex,
+        textArea,
+        placeholder = "",
+        label = "",
+        type = "text",
+        error = false,
+        errorText = "",
+        required,
+        rows = "5",
+        lastOne,
+        ...rest
+    } = props;
 
     const inputRef = useRef();
+
+    useEffect(() => {
+        required && setIsRequired("required");
+    }, [required]);
 
     function checkValue(value) {
         let regX = /[^a-zA-Z\u00C0-\u024F ]/;
@@ -14,10 +33,15 @@ const Input = (props) => {
             setInputValue(value);
         }
     }
+
+    function resetValue() {
+        setTimeout(() => setInputValue(""), 200);
+    }
+
     return (
-        <div className={`rounded-md bg-white m-2 ${wrapperClassName}`}>
+        <div className={`m-2 ${wrapperClassName}`}>
             <div
-                className={`border-4 rounded-md transition duration-500 ease-in-out ${
+                className={`border-4 bg-white rounded-md transition duration-500 ease-in-out ${
                     error ? "focus-within:border-red-600 border-red-600" : "focus-within:border-green-500 border-gray-500"
                 }`}
                 onClick={() => inputRef.current.focus()}
@@ -28,6 +52,7 @@ const Input = (props) => {
                 {textArea ? (
                     <textarea
                         rows={rows}
+                        required={isRequired}
                         name={label}
                         value={inputValue}
                         onChange={(e) => (regex ? checkValue(e.target.value) : setInputValue(e.target.value))}
@@ -40,6 +65,7 @@ const Input = (props) => {
                 ) : (
                     <input
                         value={inputValue}
+                        required={isRequired}
                         name={label}
                         onChange={(e) => (regex ? checkValue(e.target.value) : setInputValue(e.target.value))}
                         ref={inputRef}
@@ -52,6 +78,19 @@ const Input = (props) => {
                 )}
             </div>
             {errorText && <p className="text-xs pl-2 text-red-600 mb-4">{errorText}</p>}
+            {lastOne && (
+                <div className="flex flex-col p-4 bg-black">
+                    <button
+                        onClick={() => {
+                            resetValue();
+                        }}
+                        className="w-full px-3 py-4 text-white border-2 border-red-500 bg-red-700 rounded-md focus:bg-red-800 focus:outline-none blackops"
+                        type="submit"
+                    >
+                        ENVOYER
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
