657857bf8f233e2fc1f71de1c5257055c88ce66d
WadeSpectre <mbergesjulien@gmail.com>
2021-05-15 09:44:19 +0200

    ltl tweak add fb

--- src/components/SocialNav.js
+++ src/components/SocialNav.js
@@ -4,11 +4,16 @@ function SocialNav() {
             <a href="https://twitter.com/DeaMusic63?s=09" rel="noreferrer" target="_blank">
                 <i className="fab fa-twitter fa-2x m-2 hover:text-red-600 text-gray-300 ent-left" title="Twitter"></i>
             </a>
+            <a href="https://www.facebook.com/deamusic63/" rel="noreferrer" target="_blank">
+                <i className="fab fa-facebook fa-2x m-2 hover:text-red-600 text-gray-300 entrance" title="Facebook">
+                    {" "}
+                </i>
+            </a>
             <a href="https://youtube.com/c/DEAMusic63" rel="noreferrer" target="_blank">
-                <i className="fab fa-youtube fa-2x m-2 hover:text-red-600 text-gray-300 entrance"></i>
+                <i className="fab fa-youtube fa-2x m-2 hover:text-red-600 text-gray-300 entrance" title="Youtube"></i>
             </a>
             <a href="https://m.soundcloud.com/dea63" rel="noreferrer" target="_blank">
-                <i className="fab fa-soundcloud fa-2x m-2 hover:text-red-600 text-gray-300 ent-right"></i>
+                <i className="fab fa-soundcloud fa-2x m-2 hover:text-red-600 text-gray-300 ent-right" title="Soundcloud"></i>
             </a>
         </div>
     );
