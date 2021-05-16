f2875aaf42160ee12a864c9886323e45dc086155
WadeSpectre <mbergesjulien@gmail.com>
2021-05-16 09:11:56 +0200

    fix scrolltotop

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -9,8 +9,11 @@ function BackButton() {
 
     return (
         <div className="flex fixed top-3 left-2" title="Retour à l'accueil">
-            <Link to={ROUTES.HOME}>
-                <button onClick={() => scrollToTop} className="bg-black border-2 rounded-full px-3 py-2 border-gray-500 hover:border-white hover:text-gray-500 focus:outline-none">
+            <Link to={ROUTES.MUSIC}>
+                <button
+                    onClick={() => scrollToTop()}
+                    className="bg-black border-2 rounded-full px-3 py-2 border-gray-500 hover:border-white hover:text-gray-500 focus:outline-none"
+                >
                     <i className="fas fa-arrow-left fa-2x"></i>
                 </button>
             </Link>
