eb2cc06e55fdf510f2fa400a96f664f5e03579a2
WadeSpectre <mbergesjulien@gmail.com>
2021-05-18 16:51:00 +0200

    add logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="w-full text-xs text-center md:text-right text-gray-300 pt-2 pr-4 bottom">
+        <footer className="w-full text-xs text-center md:text-right pt-2 pr-4 bottom">
             <a href="https://github.com/WadeSpectre?tab=repositories" rel="noreferrer" target="_blank">
                 © 2021 WadeSpectre
             </a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,17 @@
 import { useEffect } from "react";
 
 import "../styles/Home.css";
-import { Footer } from "../components/index";
+import { Title, Footer } from "../components/index";
 
-function Home() {
+export default function Home() {
     useEffect(() => {
         document.title = "Julien Bergès | Développeur Web";
     }, []);
 
     return (
         <main className="min-h-screen flex flex-col items-center justify-center relative">
+            <Title title="HOME" />
             <Footer />
         </main>
     );
 }
-
-export default Home;
