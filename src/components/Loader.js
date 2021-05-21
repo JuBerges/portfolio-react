d549d15a52c18a233fa533c1c7470e15133e14ad
WadeSpectre <mbergesjulien@gmail.com>
2021-05-21 17:24:24 +0200

    add loading spinner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,9 @@
 export default function Loader() {
     return (
         <div className="flex h-screen justify-center items-center">
-            <div className="h-24 w-24 rounded-full loader animate-spin"></div>
+            <div className="h-24 w-24 rounded-full animate-spin text-blue-500">
+                <i class="fas fa-spinner fa-4x"></i>
+            </div>
         </div>
     );
 }
